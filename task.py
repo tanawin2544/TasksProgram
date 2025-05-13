1f674473aa669e74f118e5129896de0e40620b02
tanawin2544 <124644225+tanawin2544@users.noreply.github.com>
2025-05-13 16:30:11 +0700

    Update task.py

--- task.py
+++ task.py
@@ -9,7 +9,7 @@ import requests
 
 CURRENT_VERSION = "1.0.1"
 VERSION_URL = "https://raw.githubusercontent.com/tanawin2544/TasksProgram/main/version.txt"
-UPDATE_URL = "https://drive.google.com/uc?export=download&id=1HJwuQwIVA8godaeE2p1t1d6nbO3YU96s"
+UPDATE_URL = "https://drive.google.com/uc?export=download&id=1Av8bKGocJkSE5RQItgbFrPF3JiaubJqw"
 
 TASKS_FILE = 'tasks.json'
 
