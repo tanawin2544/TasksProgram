89bc0be1b46d2e47705a4b071f862fba12d8106e
tanawin2544 <124644225+tanawin2544@users.noreply.github.com>
2025-05-13 16:14:33 +0700

    Update task.py

--- task.py
+++ task.py
@@ -18,8 +18,8 @@ class ToDoApp:
         self.root = root
         self.root.geometry("320x400")
         self.root.attributes('-topmost', True)
-        self.root.overrideredirect(True)  # ✅ ซ่อน Title Bar ดั้งเดิม
-        self.root.iconbitmap("icon.ico")
+        #self.root.overrideredirect(True)  # ✅ ซ่อน Title Bar ดั้งเดิม
+        #self.root.iconbitmap("icon.ico")
         self.root.title("To-Do Widget")  # ให้มี icon บน taskbar
 
         # ✅ ICON
