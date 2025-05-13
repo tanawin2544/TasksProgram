1f376f5430a88a3df6bed2b6e3247b57f2d98239
tanawin2544 <124644225+tanawin2544@users.noreply.github.com>
2025-05-13 12:11:11 +0700

    Update task.py

--- task.py
+++ task.py
@@ -8,7 +8,7 @@ from datetime import datetime
 import requests
 
 CURRENT_VERSION = "1.0.0"
-VERSION_URL = "https://raw.githubusercontent.com/yourusername/todo-widget/main/version.txt"
+VERSION_URL = "https://raw.githubusercontent.com/tanawin2544/TasksProgram/refs/heads/main/version.txt"
 UPDATE_URL = "https://drive.google.com/uc?export=download&id=1HJwuQwIVA8godaeE2p1t1d6nbO3YU96s"
 
 TASKS_FILE = 'tasks.json'
